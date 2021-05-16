668c402e7d643fcc69ffeebe2ffe0fa62dd1b5e7
Harshal Patil <harshal.rp@gmail.com>
2021-05-16 18:06:47 +0530

    Validator draft - continued

--- src/domain/content/tag.ts
+++ src/domain/content/tag.ts
@@ -1,16 +1,30 @@
-import { Tag } from '@prisma/client';
+import { Tag, Prisma } from '@prisma/client';
 import slugify from '@sindresorhus/slugify';
 
 import { Context } from '../../context';
+import { Either } from '../../util/Either';
 import { isEqIgnore } from '../../util/unit';
-import { ErrorCode } from '../AppError';
+import { AppError, ErrorCode } from '../AppError';
 import { R } from '../R';
+import { collect, concat, maxLen, minLen, notEmpty } from '../validator';
 import { TagInput } from './type';
 
 
 export async function createTag({ db }: Context, newTag: TagInput): DomainResult<Tag> {
 
   const slug = slugify(newTag.name);
+
+  const validationRs = validator({
+    name: newTag.name,
+    description: newTag.description || ''
+  });
+
+  if (Either.isLeft(validationRs)) {
+    const errors = validationRs.value;
+    const message = errors.join('\n');
+
+    return R.ofError(ErrorCode.INVALID_DATA, message);
+  }
 
   // DB READ
   const matchingTags = await db.tag.findMany({
@@ -30,7 +44,7 @@ export async function createTag({ db }: Context, newTag: TagInput): DomainResult
   // DB Insert
   const tag: Tag = await db.tag.create({
     data: {
-      name: newTag.name,
+      name: newTag.name.trim(),
       slug: adjustedSlug,
       description: newTag.description || '',
       approved: false
@@ -39,3 +53,51 @@ export async function createTag({ db }: Context, newTag: TagInput): DomainResult
 
   return R.of(tag);
 }
+
+export async function updateTag(ctx: Context, tagId: string, newTag: TagInput) {
+
+  const tag = await ctx.db.tag.findUnique({
+    where: {
+      id: BigInt(tagId)
+    }
+  });
+
+  if (!tag) {
+    return R.ofError(ErrorCode.NOT_FOUND, 'Tag not found');
+  }
+
+  const validationRs = validator({
+    name: newTag.name,
+    description: newTag.description || ''
+  });
+
+  if (Either.isLeft(validationRs)) {
+    const errors = validationRs.value;
+    const message = errors.join('\n');
+
+    return R.ofError(ErrorCode.INVALID_DATA, message);
+  }
+
+  const updated = await ctx.db.tag.update({
+    where: {
+      id: tag.id
+    },
+    data: {
+      name: newTag.name.trim(),
+      description: newTag.description || '',
+      slug: newTag.slug || tag.slug
+    }
+  });
+
+  return updated;
+}
+
+
+const validator = collect<Tag, string>({
+  name: concat(
+    notEmpty('Tag should not be empty'),
+    minLen(2, 'Tag name must be two characters or more'),
+    maxLen(48, 'Tag name should not be more than 48 characters')),
+
+  description: maxLen(140, 'Description should not be more than 140 characters')
+});
---
+++ src/domain/validator.ts
@@ -0,0 +1,109 @@
+import { Either } from '../util/Either';
+
+
+export type ValidatorConfig<T, E> = {
+  [key in keyof Partial<T>]:
+    | Assertion<T[key], E>
+    | ValidatorConfig<T[key], E>;
+};
+
+export type Assertion<T, E> = (value: T) => Either<T, E[]>;
+
+
+
+export function notEmpty<E = string>(msg: E): Assertion<string, E> {
+  return ((value: string) =>
+    (value.trim().length > 0)
+      ? Either.right(value)
+      : Either.left([msg]));
+}
+
+
+export function minLen<E = string>(minLength: number, msg: E): Assertion<string, E> {
+  return (value: string) =>
+    (value.length > minLength)
+      ? Either.right(value)
+      : Either.left([msg]);
+}
+
+export function maxLen<E = string>(maxLength: number, msg: E): Assertion<string, E> {
+  return (value: string) =>
+    (value.length < maxLength)
+      ? Either.right(value)
+      : Either.left([msg]);
+}
+
+export function length<E = string>(length: number, msg: E): Assertion<string, E> {
+  return (value: string) =>
+    (value.length === length)
+      ? Either.right(value)
+      : Either.left([msg]);
+}
+
+
+export function concat<T, E>(...assertions: Assertion<T, E>[]): Assertion<T, E> {
+
+  const callback = (value: T) => {
+
+    const collect: E[] = [];
+
+    assertions.forEach((assert) => {
+      const result = assert(value);
+
+      if (Either.isLeft(result)) {
+        collect.push(...result.value);
+      }
+    });
+
+    if (collect.length) {
+      return Either.left(collect);
+    } else {
+      return Either.right(value);
+    }
+  };
+
+  return callback;
+}
+
+
+export function collect<T, E>(config: ValidatorConfig<T, E>): Assertion<Partial<T>, E> {
+
+  const callback = (value: Partial<T>) => {
+
+    function traverse<X>(config: ValidatorConfig<X, E>, obj: any, collect: E[]) {
+
+      for (const [key, asserOrConfig] of Object.entries(config)) {
+
+        const currentValue = obj[key];
+
+        if (isAssertion<X, E>(asserOrConfig)) {
+
+          const result = asserOrConfig(currentValue);
+
+          if (Either.isLeft(result)) {
+            collect.push(...result.value);
+          }
+        } else {
+          traverse(asserOrConfig as any, currentValue, collect);
+        }
+      }
+
+      return collect;
+    }
+
+    const result = traverse(config, value, []);
+
+    if (result.length) {
+      return Either.left(result);
+    } else {
+      return Either.right(value);
+    }
+  };
+
+  return callback;
+}
+
+
+function isAssertion<T, E>(value: any): value is Assertion<T, E> {
+  return typeof value === 'function';
+}
