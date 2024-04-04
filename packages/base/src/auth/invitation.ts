e9446958444056a5fe5449ae740f159159f0d91b
Shevil Mistry <shevilmistry001@gmail.com>
2024-04-04 19:24:09 +0530

    Removed unnecessary comments

--- packages/base/src/auth/invitation.ts
+++ packages/base/src/auth/invitation.ts
@@ -9,25 +9,7 @@ const InvitationClaimDTO = z.object({
     password: z.string(),
   });
 
-export async function addInvitationStrategy(app: HonoAuthApp): Promise<void> {
-    // app.post('/claim-invitation', async (c) => {
-    //     const body = await c.req.json();
-    //     const parsed = InvitationClaimDTO.safeParse(body);
-
-    //     if (!parsed.success) {
-    //     c.status(422);
-
-    //     // TODO: Error handling.
-    //     return c.json({ issues: parsed.error.issues });
-    //     }
-
-    //     const dto = parsed.data;
-    //     const result = await claimInvitation(c.var.authContext, dto.code, dto.password)
-    //     c.status(result? 200 : 400);
-
-    //     return c.json({ value: result });
-    // })
-
+export async function claimAndAcceptInvitation(app: HonoAuthApp): Promise<void> {
     app.get('/invitations/:code', async (c) => {
         const code = c.req.param('code');
         const request = await findInvitationByCode(c.var.authContext.db, code);
