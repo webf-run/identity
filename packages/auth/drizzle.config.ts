26b7dcc60a18c72de8643643ef73d3af08bce86f
Harshal Patil <harshal.rp@gmail.com>
2024-05-12 14:53:20 +0530

    fix(): update dependencies

--- packages/auth/drizzle.config.ts
+++ packages/auth/drizzle.config.ts
@@ -1,17 +1,21 @@
 import type { Config } from 'drizzle-kit';
 
 export default {
+  dialect: 'postgresql',
   schema: [
     './src/schema/identity.ts',
   ],
   out: './migrations',
-  driver: 'pg',
   dbCredentials: {
     host: 'localhost',
     port: 5432,
     user: 'postgres',
     password: 'postgres',
     database: 'base',
+  },
+  migrations: {
+    schema: 'public',
+    table: 'migrations',
   },
   verbose: true,
   strict: true,
